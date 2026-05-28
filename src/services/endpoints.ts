3d375c1d3452326b6c3246babb92ab92283fdb49
ntgiang <giangk9a1@gmail.com>
2026-05-28 09:09:48 +0700

    chore: setup axios and endpoints

---
+++ src/services/endpoints.ts
@@ -0,0 +1,9 @@
+export const API_ENDPOINTS = {
+    auth: "/api/auth",
+    users: "/api/users",
+    locations: "/api/vi-tri",
+    rooms: "/api/phong-thue",
+    bookings: "/api/dat-phong",
+    comments: "/api/binh-luan",
+} as const;
+
